78d805c45dda903de8fc4e3ae0629e540fd9069d
aliza zahid <alizayzah11@gmail.com>
2023-08-25 23:05:54 +0500

    completed

--- note taking app/script.js
+++ note taking app/script.js
@@ -2,7 +2,7 @@ const noteinput=document.getElementById("noteInput");
 const addNoteButton=document.getElementById("addNoteButton");
 const noteList=document.getElementById("noteList");
 function addnote(){
-  const notetext=noteinput.ariaValueMax.trim();//trim use todelete any white spaces
+  const notetext=noteinput.value.trim();//trim use todelete any white spaces
   if(notetext){
     noteList.innerHTML+=`<li>${notetext}<button>Delete</button></li>`;
 noteinput.value="";  
