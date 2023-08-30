e18892df7239cd59e5b620b0d1583fdad0406414
aliza zahid <alizayzah11@gmail.com>
2023-08-31 00:59:46 +0500

    draw function added

--- SIMPLE DRAWING APP/script.js
+++ SIMPLE DRAWING APP/script.js
@@ -1,8 +1,20 @@
 const canvas=document.getElementById("drawingCanvas");
-const context=document.getContext("2d");
+const context=canvas.getContext("2d");
 
 let isdrawing=false;
 canvas.addEventListener("mousedown",()=>isdrawing=true);
 canvas.addEventListener("mouseup",()=>isdrawing=false);
 canvas.addEventListener("mousemove",draw);
+
+function draw(e){
+    if(!isdrawing)  return;
+context.lineWidth=5;// Brush size
+context.lineCap="round";//Rounded line endings
+context.strokeStyle="black";//brush color
+
+   context.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop)
+   context.stroke();//draws the line
+context.beginPath();// Start a new path for the next line segment
+context.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop)
+}
 
