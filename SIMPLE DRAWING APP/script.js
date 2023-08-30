76195a1f18cd99f1ee1a89713f68c2d00aa3e4e3
aliza zahid <alizayzah11@gmail.com>
2023-08-31 00:44:59 +0500

    eventlistner added

--- SIMPLE DRAWING APP/script.js
+++ SIMPLE DRAWING APP/script.js
@@ -1,2 +1,8 @@
 const canvas=document.getElementById("drawingCanvas");
 const context=document.getContext("2d");
+
+let isdrawing=false;
+canvas.addEventListener("mousedown",()=>isdrawing=true);
+canvas.addEventListener("mouseup",()=>isdrawing=false);
+canvas.addEventListener("mousemove",draw);
+
